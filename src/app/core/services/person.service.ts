a38aa4c238b5327c5944f6f8ffa71456e580edc6
Juan Pablo Esteban <ingeniero.master26@interrapidisimo.com>
2025-04-30 08:36:49 -0500

    Wed Apr 30 08:36:49 -05 2025 ||| add simulation service

--- src/app/core/services/person.service.ts
+++ src/app/core/services/person.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { delay, Observable } from 'rxjs';
+import { delay, Observable, catchError, throwError } from 'rxjs';
 import { Person } from '../models/person.model';
 
 @Injectable({ providedIn: 'root' })
@@ -8,8 +8,12 @@ export class PersonService {
   constructor(private http: HttpClient) {}
 
   getPeople(): Observable<Person[]> {
-    return this.http
-      .get<Person[]>('src/assets/mockData/peopleData.json')
-      .pipe(delay(800));
+    return this.http.get<Person[]>('/assets/mocks/peopleData.json').pipe(
+      delay(800),
+      catchError((error) => {
+        console.error('Error fetching people:', error);
+        return throwError(() => error);
+      })
+    );
   }
 }
--- src/app/features/peopleTable/peopleTable.component.ts
+++ src/app/features/peopleTable/peopleTable.component.ts
@@ -2,16 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { PersonService } from '../../core/services/person.service';
 import { Person } from '../../core/models/person.model';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-peopleTable',
   standalone: true,
   templateUrl: './peopleTable.component.html',
   styleUrls: ['./peopleTable.component.scss'],
-  imports: [CommonModule],
+  imports: [CommonModule, HttpClientModule],
 })
 export class PeopleTableComponent implements OnInit {
   people: Person[] = [];
+  loading = false;
+  error: string | null = null;
 
   constructor(private personService: PersonService) {}
 
@@ -20,9 +23,20 @@ export class PeopleTableComponent implements OnInit {
   }
 
   loadPeople(): void {
-    this.personService.getPeople().subscribe((data) => {
-      console.log(data);
-      this.people = data;
+    this.loading = true;
+    this.error = null;
+
+    this.personService.getPeople().subscribe({
+      next: (data) => {
+        console.log('Data received:', data);
+        this.people = data;
+        this.loading = false;
+      },
+      error: (error) => {
+        console.error('Error loading people:', error);
+        this.error = 'Error al cargar los datos';
+        this.loading = false;
+      },
     });
   }
 }
